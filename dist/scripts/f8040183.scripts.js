1959c2e96160e12e3e0029e8b54f94a29541fcc0
dennissheppard <dennis.sheppard@gmail.com>
2014-09-19 09:12:32 -0500

    adding node_modules and stuff

---
+++ dist/scripts/f8040183.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("bulletinApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("bulletinApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
